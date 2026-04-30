5974a6c0cffe12eaffe75cc6c16ccb4a4883870a
GIOVANA BARBOSA SOUZA <25133664@cfp127aluno.local>
2026-04-30 15:34:50 -0300

    Api iot, subindo para colocar no render

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+let dados = []; // armazenamento em memória 
+
+app.get("/sensor", (req, res) => {
+    const {temp, hum } = req.query;
+
+    if(!temp || !hum) {
+        return res.status(400).send
+        ("Dados inválidos");
+    }
+
+    const leitura = {
+        temperatura: temp,
+        umidade: hum,
+        data: new Date()
+    };
+
+    dados.push(leitura);
+
+    console.log(leitura);
+
+    res.send("OK");
+
+}); 
+
+app.get("/dados", (req, res) => {
+    res.json(dados);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen (PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
